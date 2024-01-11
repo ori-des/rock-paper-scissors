7d7a57481c197c2ef21bc7ed009de90537ba6951
ori-des <151224246+ori-des@users.noreply.github.com>
2024-01-11 17:34:37 +0100

    Add computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+function getComputerChoice() {
+    let number = Math.floor(Math.random() * 3)
+    console.log(number)
+    if (number === 0) {
+        return "Rock";
+    } if (number === 1) {
+        return "Paper";
+    } if (number === 2) {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice())
+
