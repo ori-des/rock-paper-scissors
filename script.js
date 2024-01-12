bc128833ea0d54dd80ae71d79b008c2a37364b29
ori-des <151224246+ori-des@users.noreply.github.com>
2024-01-12 13:09:48 +0100

    Add counter

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function getComputerChoice() {
     let number = Math.floor(Math.random() * 3);
     if (number === 0) {
-        return "Rock";
+        return "rock";
     } if (number === 1) {
-        return "Paper";
+        return "paper";
     } if (number === 2) {
-        return "Scissors";
+        return "scissors";
     }
 };
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-    computerSelection = getComputerChoice().toLowerCase();
+    computerSelection = getComputerChoice();
     console.log(computerSelection)
     if (playerSelection === computerSelection) {
         return "It's a tie!";
@@ -27,7 +27,17 @@ function playRound(playerSelection, computerSelection) {
 };
 
 function game() {
-   
+    let win = 0;
+    let lose = 0;
+    let tie = 0;
+    for (let i = 0; i < 6; i++) {
+        let gameResult = playRound();
+        console.log(gameResult)
+        if (gameResult === "You Win! :)") {
+            win++;
+        }
+    } return win;
 };
 
-console.log(playRound());
+
+console.log(game());
